91557359c888af2730d70c6f7181cb66de7015b1
zyy <gt7591665@gmail.com>
2024-11-13 16:43:07 +0800

    Ch2 create quard_star board soc

---
+++ README.md
@@ -0,0 +1,5 @@
+## QuardStar Tutorial
+original author QQxiaoming
+I learn from arch-simulator-sig
+git@github.com:arch-simulator-sig/quard-star-tutorial-2021.git
+https://github.com/arch-simulator-sig/quard-star-tutorial-2021.git
--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ if [ ! -d "$SHELL_FOLDER/output/qemu" ]; then
 ./configure --prefix=$SHELL_FOLDER/output/qemu --target-list=riscv64-softmmu --enable-gtk --enable-virtfs --disable-gio
 fi
 
-make -j8
+make -j16
 make install
 
 cd ..
