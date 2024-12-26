bc44fc75bdb6c20ee0e33c55c7c3c0c1cfd2666d
zyy <gt7591665@gmail.com>
2024-12-26 19:25:14 +0800

    ch10/ch11 boot linux kernel auto

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ if [ ! -d "$SHELL_FOLDER/output/qemu" ]; then
 ./configure --prefix=$SHELL_FOLDER/output/qemu --target-list=riscv64-softmmu --enable-gtk --enable-virtfs --disable-gio
 fi
 
-#make -j16
+#make -j4
 #make install
 
 cd ..
@@ -38,7 +38,7 @@ cp -r $SHELL_FOLDER/opensbi-0.9/build/platform/quard_star/firmware/*.elf $SHELL_
 
 # generate dtb
 cd $SHELL_FOLDER/dts
-#dtc -I dts -O dtb -o $SHELL_FOLDER/output/opensbi/quard_star_sbi.dtb quard_star_sbi.dts
+dtc -I dts -O dtb -o $SHELL_FOLDER/output/opensbi/quard_star_sbi.dtb quard_star_sbi.dts
 
 # compile trusted_domain
 if [ ! -d "$SHELL_FOLDER/output/trusted_domain" ]; then  
@@ -56,8 +56,8 @@ if [ ! -d "$SHELL_FOLDER/output/uboot" ]; then
 mkdir $SHELL_FOLDER/output/uboot
 fi
 cd $SHELL_FOLDER/u-boot-2021.07
-#make CROSS_COMPILE=$CROSS_PREFIX- qemu-quard-star_defconfig
-#make CROSS_COMPILE=$CROSS_PREFIX- -j4
+make CROSS_COMPILE=$CROSS_PREFIX- qemu-quard-star_defconfig
+make CROSS_COMPILE=$CROSS_PREFIX- -j4
 cp $SHELL_FOLDER/u-boot-2021.07/u-boot $SHELL_FOLDER/output/uboot/u-boot.elf
 cp $SHELL_FOLDER/u-boot-2021.07/u-boot.map $SHELL_FOLDER/output/uboot/u-boot.map
 cp $SHELL_FOLDER/u-boot-2021.07/u-boot.bin $SHELL_FOLDER/output/uboot/u-boot.bin
@@ -65,7 +65,7 @@ $CROSS_PREFIX-objdump --source --demangle --disassemble --reloc --wide $SHELL_FO
 
 # generate uboot.dtb
 cd $SHELL_FOLDER/dts
-#dtc -I dts -O dtb -o $SHELL_FOLDER/output/uboot/quard_star_uboot.dtb quard_star_uboot.dts
+dtc -I dts -O dtb -o $SHELL_FOLDER/output/uboot/quard_star_uboot.dtb quard_star_uboot.dts
 
 
 # composite firmware
@@ -82,5 +82,55 @@ dd of=fw.bin bs=1k conv=notrunc seek=1K if=$SHELL_FOLDER/output/uboot/quard_star
 dd of=fw.bin bs=1k conv=notrunc seek=2k if=$SHELL_FOLDER/output/opensbi/fw_jump.bin
 dd of=fw.bin bs=1k conv=notrunc seek=4k if=$SHELL_FOLDER/output/trusted_domain/trusted_fw.bin
 dd of=fw.bin bs=1k conv=notrunc seek=8K if=$SHELL_FOLDER/output/uboot/u-boot.bin
+
+# build linux kernel
+if [ ! -d "$SHELL_FOLDER/output/linux_kernel" ]; then
+mkdir $SHELL_FOLDER/output/linux_kernel
+fi
+cd $SHELL_FOLDER/linux-5.15.175
+#make ARCH=riscv CROSS_COMPILE=$CROSS_PREFIX- defconfig
+#make ARCH=riscv CROSS_COMPILE=$CROSS_PREFIX- -j4
+cp $SHELL_FOLDER/linux-5.15.175/arch/riscv/boot/Image $SHELL_FOLDER/output/linux_kernel/Image
+
+# composite rootfs
+if [ ! -d "$SHELL_FOLDER/output/rootfs" ]; then
+mkdir $SHELL_FOLDER/output/rootfs
+fi
+
+if [ ! -d "$SHELL_FOLDER/output/rootfs/rootfs" ]; then
+mkdir $SHELL_FOLDER/output/rootfs/rootfs
+fi
+
+if [ ! -d "$SHELL_FOLDER/output/rootfs/bootfs" ]; then
+mkdir $SHELL_FOLDER/output/rootfs/bootfs
+fi
+
+# build linux kernel
+if [ ! -d "$SHELL_FOLDER/output/linux_kernel" ]; then
+mkdir $SHELL_FOLDER/output/linux_kernel
+fi
+cd $SHELL_FOLDER/linux-5.15.175
+#make ARCH=riscv CROSS_COMPILE=$CROSS_PREFIX- defconfig
+#make ARCH=riscv CROSS_COMPILE=$CROSS_PREFIX- -j4
+cp $SHELL_FOLDER/linux-5.15.175/arch/riscv/boot/Image $SHELL_FOLDER/output/linux_kernel/Image
+
+#  1G rootfs
+cd $SHELL_FOLDER/output/rootfs
+if [ ! -f "$SHELL_FOLDER/output/rootfs/rootfs.img" ]; then
+dd if=/dev/zero of=rootfs.img bs=1M count=1024
+#pkexec execute a shell script as another user
+#generate_rootfs.sh to format file system
+pkexec $SHELL_FOLDER/build_rootfs/generate_rootfs.sh $SHELL_FOLDER/output/rootfs/rootfs.img $SHELL_FOLDER/build_rootfs/sfdisk
+fi
+
+# bootfs-> Linux_kernel_Image
+#          uboot.dtb
+#          uboot distro script 
+cp $SHELL_FOLDER/output/linux_kernel/Image $SHELL_FOLDER/output/rootfs/bootfs/Image
+cp $SHELL_FOLDER/output/uboot/quard_star_uboot.dtb $SHELL_FOLDER/output/rootfs/bootfs/quard_star.dtb
+$SHELL_FOLDER/u-boot-2021.07/tools/mkimage -A riscv -O linux -T script -C none -a 0 -e 0 -n "Distro Boot Script" -d $SHELL_FOLDER/dts/quard_star_uboot.cmd $SHELL_FOLDER/output/rootfs/bootfs/boot.scr
+
+# copy files to filesystem
+pkexec $SHELL_FOLDER/build_rootfs/build.sh $SHELL_FOLDER/output/rootfs
 
 cd $SHELL_FOLDER
--- run.sh
+++ run.sh
@@ -7,4 +7,7 @@ $SHELL_FOLDER/output/qemu/bin/qemu-system-riscv64 \
 -smp 8 \
 -gdb tcp::26000 -D qemu.log \
 -drive if=pflash,bus=0,unit=0,format=raw,file=$SHELL_FOLDER/output/fw/fw.bin \
+-drive file=$SHELL_FOLDER/output/rootfs/rootfs.img,format=raw,id=hd0 \
+-device virtio-blk-device,drive=hd0 \
+-fw_cfg name="opt/qemu_cmdline",string="qemu_vc="$DEFAULT_V"" \
 --serial vc:$DEFAULT_VC --serial vc:$DEFAULT_VC --serial vc:$DEFAULT_VC --monitor vc:$DEFAULT_VC -display sdl --parallel none $1
